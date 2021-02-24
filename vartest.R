0c74a1d4fcabe60deecd4109761e93b5ec2c31ac
Demetris Athienitis <56597686+athienit@users.noreply.github.com>
2021-02-24 10:27:18 -0500

    update vartest

--- vartest.R
+++ vartest.R
@@ -1,4 +1,6 @@
 library(EnvStats) #make sure this package is first installed
+### Just creating fake data for the example...
+### Do not replicate, use actual data
 set.seed(1531)
 x=rnorm(30,506,3.6) #made up data
 
